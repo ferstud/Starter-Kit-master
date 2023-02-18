7fe30d535c6c25608b8dc37720fae64f5b20ee1f
fer <123444889+ferstud@users.noreply.github.com>
2023-02-18 21:21:45 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // ********************************************************* 
 // Course: TCP1101 PROGRAMMING FUNDAMENTALS 
 // Year: Trimester 1, 2022/23 (T2215) 
-// Lab: TxxL 
+// Lab: TT2L 
 // Names: FARAH NAJIHAH BINTI ROSLAN | NUR HANNANI ATHIRAH BINTI ROSLI | NUR ILHAM DAMIA BINTI TUAH 
 // IDs: 1211104391 | 1211104457 | 1211103797 
 // Emails: 1211104391@student.mmu.edu.my | 1211104457@student.mmu.edu.my| 1211103797@student.mmu.edu.my
